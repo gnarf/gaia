925c883f47b39b6d660253a837d32a8fa55eed19
Corey Frang <gnarf37@gmail.com>
2013-09-24 15:19:31 -0400

    WIP: simplify Emitter.mixin and use it on Emitter.prototype

--- apps/clock/js/emitter.js
+++ apps/clock/js/emitter.js
@@ -5,34 +5,6 @@
 // private storage for event handlers using WeakMap
 var eventMap = new WeakMap();
 
-/**
- * A simple Event Emitter prototype.  You can enhance any object to become an
- * event emitter by adding the methods to the object's prototype or object:
- *
- * MyConstructor.prototype = Object.create(Emitter.prototype)
- * -or-
- * Emitter.mixin(MyConstructor.prototype);
- * -or-
- * Emitter.mixin(MyObject);
- */
-function Emitter() {}
-
-/**
- * add Emitter prototype methods to an arbitrary object
- *
- * @param {object} target The object to decorate with the Emitter methods.
- */
-Emitter.mixin = function(target) {
-  // generate a non-enumerable property for each method
-  methods.forEach(function extendObject(method) {
-    Object.defineProperty(target, method, {
-      configurable: true,
-      value: Emitter.prototype[method]
-    });
-  });
-  return target;
-};
-
 // Internal helper: generate or return the handler array for `type` on `object`
 function getEvents(object, type) {
   var map = eventMap.get(object);
@@ -52,87 +24,126 @@ function getEvents(object, type) {
   return map[type];
 }
 
-/**
- * Bind Event Handlers
- * @param {string} type The event type to bind.
- * @param {function} handler The function to be called when event type emits.
- */
-Emitter.prototype.on = function(type, handler) {
-  var events = getEvents(this, type).handlers;
-  if (typeof handler !== 'function') {
-    throw new Error('handler must be a function');
-  }
-  events.push(handler);
-  return this;
-};
 
-/**
- * Bind Event Handler to only be called once
- * @param {string} type The event type to bind.
- * @param {function} handler The function to be called when event type emits.
- */
-Emitter.prototype.once = function(type, handler) {
-  var events = getEvents(this, type).once;
-  if (typeof handler !== 'function') {
-    throw new Error('handler must be a function');
-  }
-  events.push(handler);
-  return this;
-};
+// For use with Object.defineProperties
+var emitterMethodDescriptions = {
+  /**
+   * Bind Event Handlers
+   * @param {string} type The event type to bind.
+   * @param {function} handler The function to be called when event type emits.
+   */
+  on: {
+    configurable: true,
+    value: function Emitter_on(type, handler) {
+      var events = getEvents(this, type).handlers;
+      if (typeof handler !== 'function') {
+        throw new Error('handler must be a function');
+      }
+      events.push(handler);
+      return this;
+    }
+  },
 
-/**
- * Unbind Event Handlers
- * @param {string} [type] The event type to unbind.  Removes all event
- *                        types if omitted.
- * @param {function} [handler] The function to unbind. Removes all handlers
- *                        of the specified 'type' if omitted.
- */
-Emitter.prototype.off = function(type, handler) {
-  // remove all events if no type
-  if (!type) {
-    eventMap.delete(this);
-    return this;
-  }
-  var events = getEvents(this, type);
-  // remove all events for type if no handler
-  if (!handler) {
-    events.handlers.length = 0;
-    events.once.length = 0;
-  } else {
-    // remove only the handler
-    var index = events.handlers.indexOf(handler);
-    if (index !== -1) {
-      events.handlers.splice(index, 1);
-    } else {
-      index = events.once.indexOf(handler);
-      events.once.splice(index, 1);
+  /**
+   * Bind Event Handler to only be called once
+   * @param {string} type The event type to bind.
+   * @param {function} handler The function to be called when event type emits.
+   */
+  once: {
+    configurable: true,
+    value: function Emitter_once(type, handler) {
+      var events = getEvents(this, type).once;
+      if (typeof handler !== 'function') {
+        throw new Error('handler must be a function');
+      }
+      events.push(handler);
+      return this;
+    }
+  },
+
+  /**
+   * Unbind Event Handlers
+   * @param {string} [type] The event type to unbind.  Removes all event
+   *                        types if omitted.
+   * @param {function} [handler] The function to unbind. Removes all handlers
+   *                        of the specified 'type' if omitted.
+   */
+  off: {
+    configurable: true,
+    value: function Emitter_off(type, handler) {
+      // remove all events if no type
+      if (!type) {
+        eventMap.delete(this);
+        return this;
+      }
+      var events = getEvents(this, type);
+      // remove all events for type if no handler
+      if (!handler) {
+        events.handlers.length = 0;
+        events.once.length = 0;
+      } else {
+        // remove only the handler
+        var index = events.handlers.indexOf(handler);
+        if (index !== -1) {
+          events.handlers.splice(index, 1);
+        } else {
+          index = events.once.indexOf(handler);
+          events.once.splice(index, 1);
+        }
+      }
+      return this;
+    }
+  },
+
+  /**
+   * Emit Event
+   * @param {string} type The event type to emit.
+   * @param {object} data The first argument passed to each handler
+   *                      for the event type.
+   */
+  emit: {
+    configurable: true,
+    value: function Emitter_emit(type, data) {
+      var event;
+      var events = getEvents(this, type);
+      var allEvents = events.handlers.concat(events.once);
+      events.once.length = 0;
+
+      if (allEvents.length) {
+        while (event = allEvents.shift()) {
+          event.call(this, data);
+        }
+      }
+      return this;
     }
   }
-  return this;
 };
 
 /**
- * Emit Event
- * @param {string} type The event type to emit.
- * @param {object} data The first argument passed to each handler
- *                      for the event type.
+ * A simple Event Emitter prototype.  You can enhance any object to become an
+ * event emitter by adding the methods to the object's prototype or object:
+ *
+ * MyConstructor.prototype = Object.create(Emitter.prototype)
+ * -or-
+ * Emitter.mixin(MyConstructor.prototype);
+ * -or-
+ * Emitter.mixin(MyObject);
  */
+function Emitter() {}
 
-Emitter.prototype.emit = function(type, data) {
-  var event;
-  var events = getEvents(this, type);
-  var allEvents = events.handlers.concat(events.once);
-  events.once.length = 0;
-
-  if (allEvents.length) {
-    while (event = allEvents.shift()) {
-      event.call(this, data);
-    }
-  }
-  return this;
+/**
+ * add Emitter prototype methods to an arbitrary object
+ *
+ * @param {object} target The object to decorate with the Emitter methods.
+ */
+Emitter.mixin = function(target) {
+  // generate a non-enumerable property for each method
+  Object.defineProperties(target, emitterMethodDescriptions);
+  return target;
 };
 
-var methods = Object.keys(Emitter.prototype);
+// decorate our own prototype
+Emitter.mixin(Emitter.prototype);
 
 // export our Emitter
 exports.Emitter = Emitter;
